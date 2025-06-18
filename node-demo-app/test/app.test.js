24c9deccb815b2b4832a0a60c9a953238ac6e95c
sam-nickky <103401075+sam-nickky@users.noreply.github.com>
2025-06-19 05:25:55 +0530

    Update app.test.js

--- node-demo-app/test/app.test.js
+++ node-demo-app/test/app.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../src/index');
+const app = require('../src/app');
 
 describe('GET /health', () => {
   it('should return status ok', async () => {
