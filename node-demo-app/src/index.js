2364cfe423e28905f9c1e4f4743519f024011756
sam-nickky <103401075+sam-nickky@users.noreply.github.com>
2025-06-19 05:25:07 +0530

    Update index.js

--- node-demo-app/src/index.js
+++ node-demo-app/src/index.js
@@ -4,7 +4,8 @@ const path = require('path');
 const routes = require('./routes');
 
 dotenv.config();
-const app = express();
+//const app = express();
+const app = require('./app');
 const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '../public')));
